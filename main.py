3eb4b60b3a4aa81ba04bc19559eca62852b3a431
p1ckLL <86033747+p1ckLL@users.noreply.github.com>
2023-01-14 12:32:19 -0800

    update

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
-f = open('GalaxyThree.txt', 'r')
+f = open('theonceandfutureking.txt', 'r')
 cw = open("commonwords.txt", 'r')
 book = f.read().split()
-common_words = cw.readlines()
+common_words = cw.read().split()
+common_word_found = False
+
+cleaned_book = []
 
 for word in book:
     for common_word in common_words:
+        if word != common_word:
+          common_word_found = False
         if word == common_word:
-            book.remove(word)
-            print("word removed")
+          common_word_found = True
+    if common_word_found == False:
+      cleaned_book.append(word)
+    common_word_found = False
+
 
 print(len(book))
+print(len(cleaned_book))
 
 f.close()
