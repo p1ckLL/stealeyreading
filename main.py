760089274dc7686be95aecb66f8a43f046b40516
p1ckLL <86033747+p1ckLL@users.noreply.github.com>
2023-01-14 16:36:09 -0800

    significant progress

--- main.py
+++ main.py
@@ -1,27 +1,46 @@
-import random
+from PyDictionary import PyDictionary
+dictionary = PyDictionary()
 
-f = open('theonceandfutureking.txt', 'r')
-cw = open("commonwords.txt", 'r')
-book = f.read().split()
-common_words = cw.read().split()
+book_file = open('theonceandfutureking.txt', 'r')
+common_words = open("commonwords.txt", 'r').read().split()
 common_word_found = False
-
 cleaned_book = []
+chapter_count = 97
+page_count = 677
+pages_per_chapter = 7
+chapter = 1
+page = 1
 
-for word in book:
-    for common_word in common_words:
-        if word != common_word:
-          common_word_found = False
-        if word == common_word:
-          common_word_found = True
-          break
-    if common_word_found == False:
-      cleaned_book.append(word)
-    common_word_found = False
+def generate_chapter_info():
+  locative_info = "Chapter " + str(chapter) + ", Pages " + str(page) + "-" + str(page+pages_per_chapter-1)
+  words = generate_words()
+  
+def get_word_info(word):
+  definition = dictionary.meaning(word, disable_errors=True).values()
+  return definition
 
+def word_exists(word):
+  if dictionary.meaning(word, disable_errors=True):
+    return True
+  else:
+    return False
 
-print(len(book))
-print(len(cleaned_book))
-print(cleaned_book[random.randint(0, len(cleaned_book))])
+def generate_words():
+  words = {}
 
-f.close()
+  unpunctuated_book = book_file.read().replace(",", "").replace("\"", "").replace("!", "").replace(".", "").replace("-", "").replace("—", "").lower().split() 
+
+  for word in unpunctuated_book:
+      for common_word in common_words:
+          if word != common_word:
+            common_word_found = False
+          if word == common_word or len(word) < 9:
+            common_word_found = True
+            break
+      if common_word_found == False and word_exists(word):
+        words[word] = get_word_info(word)
+        cleaned_book.append(word)
+      common_word_found = False
+  return words
+
+book_file.close()
